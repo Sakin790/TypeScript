7b7f752107c8394e7fd2a0dcae6658b7b8366f7e
Sakin790 <sakinislam79@gmail.com>
2024-04-08 22:54:13 +0600

    function

---
+++ 01_Basics/index.js
@@ -0,0 +1,4 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+addNumbers(20, "20"); //Argument of type 'string' is not assignable to parameter of type 'number'.
---
+++ 01_Basics/index.ts
@@ -0,0 +1,5 @@
+
+function addNumbers(num1: number, num2: number) {
+    return num1 + num2
+}
+addNumbers(20,"20") //Argument of type 'string' is not assignable to parameter of type 'number'.
