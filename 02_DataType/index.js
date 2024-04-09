44eb6fb7261bfc8e6ee7f45ba9595a24fcfa1f1a
Sakin790 <sakinislam79@gmail.com>
2024-04-09 11:16:12 +0600

    datatype

---
+++ 02_DataType/index.js
@@ -0,0 +1,7 @@
+//Build in dataType => number, String, boolean , void , null, undefined
+var score;
+score = 12;
+console.log(score);
+var usename = "Mahidari";
+var isActive = false;
+var success;
---
+++ 02_DataType/index.ts
@@ -0,0 +1,9 @@
+//Build in dataType => number, String, boolean , void , null, undefined
+
+let score : number;
+score= 12
+console.log(score)
+
+let usename : string = "Mahidari"
+let isActive : boolean = false
+let success :any 
